af80a2d0eb6d779376eb2bf42253573ac9304031
Gabriele Cesa <gabriele.cesa@gmail.com>
2022-08-12 11:26:44 +0200

    r2upsamplingç testing and docs

--- escnn/nn/modules/r2upsampling.py
+++ escnn/nn/modules/r2upsampling.py
@@ -23,7 +23,7 @@ class R2Upsampling(EquivariantModule):
     def __init__(self,
                  in_type: FieldType,
                  scale_factor: Optional[int] = None,
-                 size: Optional[Union[int, Tuple[int, int], Tuple[int, int, int]]] = None,
+                 size: Optional[Union[int, Tuple[int, int]]] = None,
                  mode: str = "bilinear",
                  align_corners: bool = False
                  ):
@@ -34,6 +34,13 @@ class R2Upsampling(EquivariantModule):
         Only ``"bilinear"`` and ``"nearest"`` methods are supported.
         However, ``"nearest"`` is not equivariant; using this method may result in broken equivariance.
         For this reason, we suggest to use ``"bilinear"`` (default value).
+
+        ..warning ::
+            The module supports a ``size`` parameter as an alternative to ``scale_factor``.
+            However, the use of ``scale_factor`` should be *preferred*, since it guarantees both axes are scaled
+            uniformly, which preserves rotation equivariance.
+            A misuse of the parameter ``size`` can break the overall equivariance, since it might scale the two axes by
+            two different factors.
         
         .. warning ::
             Even if the input tensor has a `coords` attribute, the output of this module will not have one.
@@ -41,8 +48,8 @@ class R2Upsampling(EquivariantModule):
         
         Args:
             in_type (FieldType): the input field type
-            size (int or Tuple[int, int] or Tuple[int, int, int]): output spatial size.
-            scale_factor (int): multiplier for spatial size
+            size (optional, int or tuple): output spatial size.
+            scale_factor (optional, int): multiplier for spatial size
             mode (str): algorithm used for upsampling: ``nearest`` | ``bilinear``. Default: ``bilinear``
             align_corners (bool): if ``True``, the corner pixels of the input and output tensors are aligned, and thus
                     preserving the values at those pixels. This only has effect when mode is ``bilinear``.
@@ -58,8 +65,12 @@ class R2Upsampling(EquivariantModule):
         self.space = in_type.gspace
         self.in_type = in_type
         self.out_type = in_type
+
+        assert size is None or scale_factor is None, \
+            f'Only one of "size" and "scale_factor" can be set, but found scale_factor={scale_factor} and size={size}'
         
-        self._size = size
+        self._size = (size, size) if isinstance(size, int) else size
+        assert self._size is None or (isinstance(self._size, tuple) and len(self._size) ==2), self._size
         self._scale_factor = scale_factor
         self._mode = mode
         self._align_corners = align_corners if mode != "nearest" else None
