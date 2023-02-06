9c8001fd1b69ff9173f2712cb48a85d5eb458343
Peter Steinbach <p.steinbach@hzdr.de>
2023-02-06 12:01:50 +0100

    fix numpy version for deprecation of np.float, np.int, np.bool etc
    Signed-off-by: Peter Steinbach <p.steinbach@hzdr.de>

--- setup.py
+++ setup.py
@@ -1,49 +1,51 @@
-
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 about = {}
 with open("escnn/__about__.py") as fp:
     exec(fp.read(), about)
 
+# TODO: might be better to read these required packages
+#      directly from requirements.txt
 install_requires = [
-    'torch',
-    'numpy',
-    'scipy',
-    'lie_learn',
-    'joblib',
-    'pymanopt',
-    'autograd',
-    'py3nj'
+    "torch>=1.3",
+    "numpy<1.24",
+    "scipy",
+    "lie_learn",
+    "joblib",
+    "pymanopt",
+    "autograd",
+    "py3nj",
 ]
 
 
-setup_requires = []
-tests_require = ['scikit-learn', 'scikit-image']
+setup_requires = [""]
+tests_require = ["scikit-learn", "scikit-image"]
 
-with open("README.md", 'r', encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-download_url = 'https://github.com/QUVA-Lab/escnn/archive/v{}.tar.gz'.format(about['__version__'])
+download_url = "https://github.com/QUVA-Lab/escnn/archive/v{}.tar.gz".format(
+    about["__version__"]
+)
 
 setup(
-    name=about['__title__'],
-    version=about['__version__'],
-    description=about['__summary__'],
-    author=about['__author__'],
-    author_email=about['__email__'],
-    url=about['__url__'],
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
+    url=about["__url__"],
     download_url=download_url,
-    license=about['__license__'],
+    license=about["__license__"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=['test', 'test.*']),
-    python_requires='>=3.7',
+    packages=find_packages(exclude=["test", "test.*"]),
+    python_requires=">=3.7",
     keywords=[
-        'pytorch',
-        'cnn',
-        'convolutional-networks'
-        'equivariant',
-        'isometries',
+        "pytorch",
+        "cnn",
+        "convolutional-networks" "equivariant",
+        "isometries",
     ],
     install_requires=install_requires,
     setup_requires=setup_requires,
