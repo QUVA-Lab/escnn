optional normalization FT kernel in Fourier non-linearities, fixed tests

escnn/nn/modules/nonlinearities/fourier.py
@@ -41,6 +41,7 @@ class FourierPointwise(EquivariantModule):
             function: str = 'p_relu',
             inplace: bool = True,
             out_irreps: List = None,
+            normalize: bool = True,
             **grid_kwargs
     ):
         r"""
@@ -86,6 +87,7 @@ class FourierPointwise(EquivariantModule):
             *grid_args: parameters used to construct the discretization grid
             inplace (bool): applies the non-linear activation in-place. Default: `True`
             out_irreps (list, optional): optionally, one can specify a different band-limiting in output
+            normalize (bool, optional): if ``True``, the rows of the IFT matrix (and the columns of the FT matrix) are normalized. Default: ``True``
             **grid_kwargs: keyword parameters used to construct the discretization grid
             
         """
@@ -124,8 +126,9 @@ class FourierPointwise(EquivariantModule):
         
         kernel = _build_kernel(G, irreps)
         assert kernel.shape[0] == self.rho.size
-        
-        kernel = kernel / np.linalg.norm(kernel)
+
+        if normalize:
+            kernel = kernel / np.linalg.norm(kernel)
         kernel = kernel.reshape(-1, 1)
         
         grid = G.grid(*grid_args, **grid_kwargs)
@@ -144,7 +147,8 @@ class FourierPointwise(EquivariantModule):
             kernel_out = _build_kernel(G, out_irreps + _missing_input_irreps)
             assert kernel_out.shape[0] == rho_out_extended.size
 
-            kernel_out = kernel_out / np.linalg.norm(kernel_out)
+            if normalize:
+                kernel_out = kernel_out / np.linalg.norm(kernel_out)
             kernel_out = kernel_out.reshape(-1, 1)
 
             A_out = np.concatenate(
@@ -205,7 +209,7 @@ class FourierPointwise(EquivariantModule):
 
         return (b, self.out_type.size, *spatial_shape)
 
-    def check_equivariance(self, atol: float = 1e-5, rtol: float = 2e-2) -> List[Tuple[Any, float]]:
+    def check_equivariance(self, atol: float = 1e-5, rtol: float = 2e-2, assert_raise: bool = True) -> List[Tuple[Any, float]]:
     
         c = self.in_type.size
         B = 128
@@ -247,13 +251,17 @@ class FourierPointwise(EquivariantModule):
             relerr = errs / norm
 
             # print(el, errs.max(), errs.mean(), relerr.max(), relerr.min())
-        
-            assert relerr.mean()+ relerr.std() < rtol, \
-                'The error found during equivariance check with element "{}" is too high: max = {}, mean = {}, std ={}' \
-                    .format(el, relerr.max(), relerr.mean(), relerr.std())
-            errors.append((el, errs.mean()))
 
-        return errors
+            if assert_raise:
+                assert relerr.mean()+ relerr.std() < rtol, \
+                    'The error found during equivariance check with element "{}" is too high: max = {}, mean = {}, std ={}' \
+                        .format(el, relerr.max(), relerr.mean(), relerr.std())
+
+            # errors.append((el, errs.mean()))
+            errors.append(relerr)
+
+        # return errors
+        return np.concatenate(errors).reshape(-1)
 
 
 class FourierELU(FourierPointwise):
@@ -261,7 +269,7 @@ class FourierELU(FourierPointwise):
     def __init__(
             self,
             gspace: GSpace, channels: int, irreps: List, *grid_args,
-            inplace: bool = True, out_irreps: List = None,  **grid_kwargs
+            inplace: bool = True, out_irreps: List = None, normalize: bool = True,  **grid_kwargs
     ):
         r"""
 
@@ -277,11 +285,12 @@ class FourierELU(FourierPointwise):
             *grid_args: parameters used to construct the discretization grid
             inplace (bool): applies the non-linear activation in-place. Default: `True`
             out_irreps (list, optional): optionally, one can specify a different band-limiting in output
+            normalize (bool, optional): if ``True``, the rows of the IFT matrix (and the columns of the FT matrix) are normalized. Default: ``True``
             **grid_kwargs: keyword parameters used to construct the discretization grid
 
         """
         
         super(FourierELU, self).__init__(
             gspace, channels, irreps, *grid_args,
-            function='p_elu', inplace=inplace, out_irreps=out_irreps, **grid_kwargs)
+            function='p_elu', inplace=inplace, out_irreps=out_irreps, normalize=normalize, **grid_kwargs)
 

escnn/nn/modules/nonlinearities/fourier_quotient.py
@@ -43,6 +43,7 @@ class QuotientFourierPointwise(EquivariantModule):
                  function: str = 'p_relu',
                  inplace: bool=True,
                  out_irreps: List = None,
+                 normalize: bool = True,
                  **grid_kwargs
                  ):
         r"""
@@ -84,16 +85,13 @@ class QuotientFourierPointwise(EquivariantModule):
             interpreted as a function over :math:`G` (as we do here when sampling), the function will be constant along
             each coset, i.e. :math:`f(gh) = f(g)` if :math:`g \in G, h\in H`.
             An approximately uniform sampling grid over :math:`G` creates an approximately uniform grid over :math:`G/H`
-            through projection but might contain redundant elements (if the grid contains :math:`g 'in G`, any element
+            through projection but might contain redundant elements (if the grid contains :math:`g \in G`, any element
             :math:`gh` in the grid will be redundant).
             It is therefore advised to create a grid directly in the quotient space, e.g. using
             :meth:`escnn.group.SO3.sphere_grid`, :meth:`escnn.group.O3.sphere_grid`.
             We do not support yet a general method and interface to generate grids over any homogeneous space for any
             group, so you should check each group's methods.
-        
-        .. todo::
-            Mention the normalization of the transform we use
-        
+
         Args:
             gspace (GSpace):  the gspace describing the symmetries of the data. The Fourier transform is
                               performed over the group ```gspace.fibergroup```
@@ -107,6 +105,7 @@ class QuotientFourierPointwise(EquivariantModule):
                     By default (``'p_relu'``), ReLU is used.
             inplace (bool): applies the non-linear activation in-place. Default: `True`
             out_irreps (list, optional): optionally, one can specify a different band-limiting in output
+            normalize (bool, optional): if ``True``, the rows of the IFT matrix (and the columns of the FT matrix) are normalized. Default: ``True``
             **grid_kwargs: keyword parameters used to construct the discretization grid
             
         """
@@ -145,8 +144,9 @@ class QuotientFourierPointwise(EquivariantModule):
         
         kernel = _build_kernel(G, subgroup_id, irreps)
         assert kernel.shape[0] == self.rho.size
-        
-        kernel = kernel / np.linalg.norm(kernel)
+
+        if normalize:
+            kernel = kernel / np.linalg.norm(kernel)
         kernel = kernel.reshape(-1, 1)
         
         if grid is None:
@@ -162,11 +162,13 @@ class QuotientFourierPointwise(EquivariantModule):
         if out_irreps is not None:
 
             _missing_input_irreps = list(set(irreps).difference(set(out_irreps)))
+            # _missing_input_irreps = []
             rho_out_extended = G.spectral_quotient_representation(subgroup_id, *out_irreps, *_missing_input_irreps, name=None)
             kernel_out = _build_kernel(G, subgroup_id, out_irreps + _missing_input_irreps)
             assert kernel_out.shape[0] == rho_out_extended.size
 
-            kernel_out = kernel_out / np.linalg.norm(kernel_out)
+            if normalize:
+                kernel_out = kernel_out / np.linalg.norm(kernel_out)
             kernel_out = kernel_out.reshape(-1, 1)
 
             A_out = np.concatenate(
@@ -227,7 +229,7 @@ class QuotientFourierPointwise(EquivariantModule):
 
         return (b, self.out_type.size, *spatial_shape)
 
-    def check_equivariance(self, atol: float = 1e-5, rtol: float = 2e-2) -> List[Tuple[Any, float]]:
+    def check_equivariance(self, atol: float = 1e-5, rtol: float = 2e-2, assert_raise: bool = True) -> List[Tuple[Any, float]]:
     
         c = self.in_type.size
         B = 64
@@ -268,14 +270,16 @@ class QuotientFourierPointwise(EquivariantModule):
             relerr = errs / norm
             
             # print(el, errs.max(), errs.mean(), relerr.max(), relerr.min())
+            if assert_raise:
+                assert relerr.mean()+ relerr.std() < rtol, \
+                    'The error found during equivariance check with element "{}" is too high: max = {}, mean = {}, std ={}, maxerr={}, xmean={}, xstd={}' \
+                        .format(el, relerr.max(), relerr.mean(), relerr.std(), errs[np.argmax(relerr)], out1.mean(), out1.std())
         
-            assert relerr.mean()+ relerr.std() < rtol, \
-                'The error found during equivariance check with element "{}" is too high: max = {}, mean = {}, std ={}, maxerr={}, xmean={}, xstd={}' \
-                    .format(el, relerr.max(), relerr.mean(), relerr.std(), errs[np.argmax(relerr)], out1.mean(), out1.std())
-        
-            errors.append((el, errs.mean()))
-    
-        return errors
+            # errors.append((el, errs.mean()))
+            errors.append(relerr)
+
+        # return errors
+        return np.concatenate(errors)
 
 
 class QuotientFourierELU(QuotientFourierPointwise):
@@ -289,6 +293,7 @@ class QuotientFourierELU(QuotientFourierPointwise):
                  grid: List[GroupElement] = None,
                  inplace: bool = True,
                  out_irreps: List = None,
+                 normalize: bool = True,
                  **grid_kwargs
                  ):
         r"""
@@ -305,8 +310,9 @@ class QuotientFourierELU(QuotientFourierPointwise):
             irreps (list): list of irreps' ids to construct the band-limited representation
             *grid_args: parameters used to construct the discretization grid
             grid (list, optional): list containing the elements of the group to use for sampling. Optional (default ``None``).
-            inplace (bool): applies the non-linear activation in-place. Default: `True`
+            inplace (bool): applies the non-linear activation in-place. Default: ``True``
             out_irreps (list, optional): optionally, one can specify a different band-limiting in output
+            normalize (bool, optional): if ``True``, the rows of the IFT matrix (and the columns of the FT matrix) are normalized. Default: ``True``
             **grid_kwargs: keyword parameters used to construct the discretization grid
 
         """
@@ -317,6 +323,7 @@ class QuotientFourierELU(QuotientFourierPointwise):
             inplace=inplace,
             grid=grid,
             out_irreps=out_irreps,
+            normalize=normalize,
             **grid_kwargs
         )
 

test/nn/test_fourier.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest import TestCase
 
+import matplotlib.pyplot as plt
+
 import escnn.nn.init as init
 from escnn.nn import *
 from escnn.gspaces import *
@@ -24,7 +26,7 @@ class TestFourier(TestCase):
             print(F, grid['N'])
 
             cl = FourierELU(g, 3, [(l,) for l in range(F+1)], **grid)
-            cl.check_equivariance()
+            cl.check_equivariance(rtol=3e-2)
 
     def test_o2(self):
         g = no_base_space(o2_group(10))
@@ -98,7 +100,7 @@ class TestFourier(TestCase):
             print(n, F, len(grid))
         
             cl = QuotientFourierELU(g, n, 3, [(l,) for l in range(F + 1)], grid=grid)
-            cl.check_equivariance()
+            cl.check_equivariance(rtol=3e-2)
 
     def test_so3_sphere(self):
         g = no_base_space(so3_group(1))
@@ -143,6 +145,9 @@ class TestFourier(TestCase):
                 'type': 'regular',
                 'N': N
             }
+
+            print(F, N)
+
             cl = FourierELU(g, 3, g.fibergroup.bl_irreps(F), out_irreps=g.fibergroup.bl_irreps(0), **grid)
             cl.check_equivariance()
 
@@ -174,23 +179,29 @@ class TestFourier(TestCase):
 
         grid = g.fibergroup.sphere_grid(type='ico')
         cl = QuotientFourierELU(g, (False, -1), 3, g.fibergroup.bl_irreps(2), out_irreps=g.fibergroup.bl_irreps(0), grid=grid)
-        cl.check_equivariance(rtol=1e-1)
+        # cl.check_equivariance(rtol=1e-1)
+        cl.check_equivariance(rtol=5e-2)
         cl = QuotientFourierELU(g, (False, -1), 3, g.fibergroup.bl_irreps(2), out_irreps=g.fibergroup.bl_irreps(1), grid=grid)
-        cl.check_equivariance(rtol=1e-1)
+        # cl.check_equivariance(rtol=1e-1)
+        cl.check_equivariance(rtol=5e-2)
 
         grid = g.fibergroup.sphere_grid(type='thomson_cube', N=1)
         cl = QuotientFourierELU(g, (False, -1), 3, g.fibergroup.bl_irreps(2), out_irreps=g.fibergroup.bl_irreps(0), grid=grid)
-        cl.check_equivariance(rtol=1e-1)
+        # cl.check_equivariance(rtol=1e-1)
+        cl.check_equivariance(rtol=5e-2)
         cl = QuotientFourierELU(g, (False, -1), 3, g.fibergroup.bl_irreps(2), out_irreps=g.fibergroup.bl_irreps(1), grid=grid)
-        cl.check_equivariance(rtol=1e-1)
+        # cl.check_equivariance(rtol=1e-1)
+        cl.check_equivariance(rtol=5e-2)
 
         cl = FourierELU(g, 3, g.fibergroup.bl_irreps(1), out_irreps=g.fibergroup.bl_irreps(0), type='thomson_cube', N=1)
-        cl.check_equivariance(rtol=1e-1)
+        # cl.check_equivariance(rtol=1e-1)
+        cl.check_equivariance(rtol=6e-2)
 
         cl = FourierELU(g, 3, g.fibergroup.bl_irreps(2), out_irreps=g.fibergroup.bl_irreps(0), type='thomson_cube', N=3)
-        cl.check_equivariance(rtol=1e-1)
+        # cl.check_equivariance(rtol=1e-1)
+        cl.check_equivariance(rtol=7e-2)
 
-        cl = FourierELU(g, 3, g.fibergroup.bl_irreps(2), out_irreps=g.fibergroup.bl_irreps(1), type='thomson_cube', N=3)
+        cl = FourierELU(g, 3, g.fibergroup.bl_irreps(2), out_irreps=g.fibergroup.bl_irreps(1), type='thomson_cube', N=4)
         cl.check_equivariance(rtol=1e-1)
 
         for F, N in zip(range(1, 3), [24, 120]):
@@ -201,7 +212,8 @@ class TestFourier(TestCase):
             print(F, N)
 
             cl = FourierELU(g, 3, g.fibergroup.bl_irreps(max(0, F - 1)), out_irreps=g.fibergroup.bl_irreps(F), **grid)
-            cl.check_equivariance(rtol=1e-1)
+            # cl.check_equivariance(rtol=1e-1)
+            cl.check_equivariance(rtol=5e-2)
 
             cl = FourierELU(g, 3, g.fibergroup.bl_irreps(F), out_irreps=g.fibergroup.bl_irreps(0), **grid)
             cl.check_equivariance(rtol=1e-1)
@@ -251,13 +263,13 @@ class TestFourier(TestCase):
             ]
 
             cl = QuotientFourierELU(g, n, 3, g.fibergroup.bl_irreps(F), out_irreps=g.fibergroup.bl_irreps(0), grid=grid)
-            cl.check_equivariance()
+            cl.check_equivariance(rtol=3e-2)
 
             cl = QuotientFourierELU(g, n, 3, g.fibergroup.bl_irreps(F), out_irreps=g.fibergroup.bl_irreps(max(0, F - 2)), grid=grid)
-            cl.check_equivariance()
+            cl.check_equivariance(rtol=3e-2)
 
             cl = QuotientFourierELU(g, n, 3, g.fibergroup.bl_irreps(max(0, F - 2)), out_irreps=g.fibergroup.bl_irreps(F), grid=grid)
-            cl.check_equivariance()
+            cl.check_equivariance(rtol=3e-2)
 
     def test_so3_sphere_different_out(self):
         g = no_base_space(so3_group(1))
@@ -285,7 +297,8 @@ class TestFourier(TestCase):
             print(F, len(grid))
 
             cl = QuotientFourierELU(g, ('cone', -1), 3, g.fibergroup.bl_irreps(F), out_irreps=g.fibergroup.bl_irreps(0), grid=grid)
-            cl.check_equivariance(rtol=1e-1, atol=1e-2)
+            # cl.check_equivariance(rtol=1e-1, atol=1e-2)
+            cl.check_equivariance(rtol=5e-2, atol=1e-2)
 
             cl = QuotientFourierELU(g, ('cone', -1), 3, g.fibergroup.bl_irreps(F), out_irreps=g.fibergroup.bl_irreps(max(0, F - 2)), grid=grid)
             cl.check_equivariance(rtol=1e-1)
@@ -323,14 +336,23 @@ class TestFourier(TestCase):
 
     def test_norm_preserved_relu(self):
 
+        # Check if the norm of the features is approximately preserved
+
+        # To do so, we try to apply ReLU over strictly positive features
+        # or apply ReLU over roughly centered features (expecting the norm to reduce by approx sqrt(2))
+
+        # for these tests to work, we should not apply normalization of the Fourier matrices, otherwise the scale
+        # of the output features depends on the number of Fourier components recovered
+
         g = no_base_space(so3_group(1))
 
         for F, N in zip(range(1, 4), [24, 120, 300]):
+            print(F, N)
 
-            cl = FourierPointwise(g, 1, g.fibergroup.bl_irreps(F), function='p_relu', type='thomson', N=N)
+            cl = FourierPointwise(g, 1, g.fibergroup.bl_irreps(F), normalize=False, function='p_relu', type='thomson', N=N)
 
             c = cl.in_type.size
-            B = 128
+            B = 256
 
             # make sure the average value of the feature is very high so the ReLU acts as an identity
             # then, check that the norm of the input and output features are similar
@@ -368,16 +390,18 @@ class TestFourier(TestCase):
             y = cl(x).tensor
             y = y.view(B, len(cl.out_type), cl.rho_out.size)
             out_norms = torch.linalg.norm(y, dim=2).reshape(-1).cpu().detach().numpy()
-            norms_ratio = in_norms / out_norms
-            self.assertTrue(np.allclose(norms_ratio, np.sqrt(2), atol=3e-1, rtol=1e-1), msg=f"{np.fabs(norms_ratio-np.sqrt(2)).max()}")
+            norms_ratio = out_norms / in_norms
+            print(norms_ratio.max(), norms_ratio.min(), norms_ratio.mean(), norms_ratio.std())
+            self.assertTrue(np.allclose(norms_ratio, 1./np.sqrt(2), atol=1e-1, rtol=1e-1), msg=f"{np.fabs(norms_ratio-1./np.sqrt(2)).max()}")
 
-        for F, N in zip(range(1, 4), [8, 17, 33]):
+        for F, N in zip(range(1, 4), [16, 34, 66]):
+            print(F, N)
 
             grid = g.fibergroup.sphere_grid(type='thomson', N=N)
-            cl = QuotientFourierPointwise(g, (False, -1), 1, g.fibergroup.bl_irreps(F), function='p_relu', grid=grid)
+            cl = QuotientFourierPointwise(g, (False, -1), 40, g.fibergroup.bl_irreps(F), normalize=False, function='p_relu', grid=grid)
 
             c = cl.in_type.size
-            B = 128
+            B = 32
 
             # make sure the average value of the feature is very high so the ReLU acts as an identity
             # then, check that the norm of the input and output features are similar
@@ -389,13 +413,12 @@ class TestFourier(TestCase):
                 if irr.is_trivial():
                     x[:, :, p] = 150.
                 p += irr.size
-
-            in_norms = torch.linalg.norm(x, dim=2).reshape(-1).cpu().detach().numpy()
             x = x.view(B, cl.in_type.size)
+
+            in_norms = torch.linalg.norm(x, dim=1).reshape(-1).cpu().detach().numpy()
             x = cl.in_type(x).transform_fibers(g.fibergroup.sample())
             y = cl(x).tensor
-            y = y.view(B, len(cl.out_type), cl.rho_out.size)
-            out_norms = torch.linalg.norm(y, dim=2).reshape(-1).cpu().detach().numpy()
+            out_norms = torch.linalg.norm(y, dim=1).reshape(-1).cpu().detach().numpy()
             self.assertTrue(np.allclose(in_norms, out_norms))
 
             # now, make sure features are centered around zero such that, on average, half of the entries are set to 0
@@ -409,14 +432,115 @@ class TestFourier(TestCase):
                     x[:, :, p] = 0.
                 p += irr.size
 
-            in_norms = torch.linalg.norm(x, dim=2).reshape(-1).cpu().detach().numpy()
             x = x.view(B, cl.in_type.size)
+            in_norms = torch.linalg.norm(x, dim=1).reshape(-1).cpu().detach().numpy()
             x = cl.in_type(x).transform_fibers(g.fibergroup.sample())
             y = cl(x).tensor
-            y = y.view(B, len(cl.out_type), cl.rho_out.size)
-            out_norms = torch.linalg.norm(y, dim=2).reshape(-1).cpu().detach().numpy()
-            norms_ratio = in_norms / out_norms
-            self.assertTrue(np.allclose(norms_ratio, np.sqrt(2), atol=4e-1, rtol=1e-1), msg=f"{np.fabs(norms_ratio-np.sqrt(2)).max()}")
+            out_norms = torch.linalg.norm(y, dim=1).reshape(-1).cpu().detach().numpy()
+            norms_ratio = out_norms / in_norms
+            # print(norms_ratio.max(), norms_ratio.min(), norms_ratio.mean(), norms_ratio.std())
+            self.assertTrue(np.allclose(norms_ratio, 1. / np.sqrt(2), atol=1e-1, rtol=1e-1), msg=f"{np.fabs(norms_ratio-1./np.sqrt(2)).max()}")
+
+        max_f = 5
+        for F, N in zip(range(1, max_f+1), [200]*max_f):
+            print(F, N)
+
+            grid = g.fibergroup.sphere_grid(type='thomson', N=N)
+            cl = QuotientFourierPointwise(g, (False, -1), 30, g.fibergroup.bl_irreps(1), out_irreps=g.fibergroup.bl_irreps(F), normalize=False, function='p_relu', grid=grid)
+
+            c = cl.in_type.size
+            B = 32
+
+            # make sure the average value of the feature is very high so the ReLU acts as an identity
+            # then, check that the norm of the input and output features are similar
+            x = torch.randn(B, c)
+            x = x.view(B, len(cl.in_type), cl.rho.size)
+            p = 0
+            for irr in cl.rho.irreps:
+                irr = g.irrep(*irr)
+                if irr.is_trivial():
+                    x[:, :, p] = 150.
+                p += irr.size
+            x = x.view(B, cl.in_type.size)
+
+            in_norms = torch.linalg.norm(x, dim=1).reshape(-1).cpu().detach().numpy()
+            x = cl.in_type(x).transform_fibers(g.fibergroup.sample())
+            y = cl(x).tensor
+            out_norms = torch.linalg.norm(y, dim=1).reshape(-1).cpu().detach().numpy()
+            self.assertTrue(np.allclose(in_norms, out_norms))
+
+            # now, make sure features are centered around zero such that, on average, half of the entries are set to 0
+            # by ReLU. We expect the output to have a norm roughly sqrt(2) smaller than the input
+            x = torch.randn(B, c)
+            x = x.view(B, len(cl.in_type), cl.rho.size)
+            p = 0
+            for irr in cl.rho.irreps:
+                irr = g.irrep(*irr)
+                if irr.is_trivial():
+                    x[:, :, p] = 0.
+                p += irr.size
+
+            x = x.view(B, cl.in_type.size)
+            in_norms = torch.linalg.norm(x, dim=1).reshape(-1).cpu().detach().numpy()
+            x = cl.in_type(x).transform_fibers(g.fibergroup.sample())
+            y = cl(x).tensor
+            out_norms = torch.linalg.norm(y, dim=1).reshape(-1).cpu().detach().numpy()
+            norms_ratio = out_norms / in_norms
+            self.assertTrue(np.allclose(norms_ratio, 1. / np.sqrt(2), atol=1e-1, rtol=1e-1), msg=f"{np.fabs(norms_ratio-1./np.sqrt(2)).max()}")
+
+    def plot_errors_so3(self):
+        g = no_base_space(so3_group(1))
+
+        Fs = list(range(1, 4))
+        Ns = list(range(20, 230, 10))
+
+        errors_mean = np.zeros((len(Fs), len(Ns)))
+        errors_std = np.zeros((len(Fs), len(Ns)))
+
+        for i, F in enumerate(Fs):
+            print(F)
+            for j, N in enumerate(Ns):
+                cl = FourierPointwise(g, 1, g.fibergroup.bl_irreps(F), normalize=False, function='p_relu', type='thomson', N=N)
+                errs = cl.check_equivariance(rtol=1e-1, assert_raise=False)
+
+                errors_mean[i, j] = errs.mean()
+                errors_std[i, j] = errs.std()
+
+        fig, ax = plt.subplots()
+        for i, F in enumerate(Fs):
+            plt.plot(Ns, errors_mean[i, :], label=f'{F}')
+            plt.fill_between(Ns, errors_mean[i, :] - errors_std[i, :], errors_mean[i, :] + errors_std[i, :], alpha=0.3)
+
+        plt.ylim([0., 0.3])
+        plt.legend()
+        plt.show()
+
+    def plot_errors_sphere(self):
+        g = no_base_space(so3_group(1))
+
+        Fs = list(range(1, 5))
+        Ns = list(range(8, 88, 8))
+
+        errors_mean = np.zeros((len(Fs), len(Ns)))
+        errors_std = np.zeros((len(Fs), len(Ns)))
+
+        for i, F in enumerate(Fs):
+            print(F)
+            for j, N in enumerate(Ns):
+                cl = QuotientFourierPointwise(g, (False, -1), 1, g.fibergroup.bl_irreps(F), normalize=False, function='p_relu', grid=g.fibergroup.sphere_grid(type='thomson', N=N))
+                errs = cl.check_equivariance(rtol=1e-1, assert_raise=False)
+
+                errors_mean[i, j] = errs.mean()
+                errors_std[i, j] = errs.std()
+
+        fig, ax = plt.subplots()
+        for i, F in enumerate(Fs):
+            plt.plot(Ns, errors_mean[i, :], label=f'{F}')
+            plt.fill_between(Ns, errors_mean[i, :] - errors_std[i, :], errors_mean[i, :] + errors_std[i, :], alpha=0.3)
+
+        plt.ylim([0., 0.2])
+        plt.legend()
+        plt.show()
 
 
 if __name__ == '__main__':