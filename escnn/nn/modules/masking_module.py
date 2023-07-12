acea54418ba467454ae500606c6a4ea65b441407
Kale Kundert <kale@thekunderts.net>
2023-07-12 14:46:12 -0400

    Generalize MaskModule to N-dimensional input
    Fixes: #62
    
    Signed-off-by: Kale Kundert <kale@thekunderts.net>

--- escnn/nn/modules/masking_module.py
+++ escnn/nn/modules/masking_module.py
@@ -3,6 +3,7 @@ from escnn.nn import GeometricTensor
 from escnn.nn import FieldType
 from .equivariant_module import EquivariantModule
 
+from itertools import product, repeat
 from typing import Tuple
 
 import torch
@@ -12,30 +13,44 @@ import math
 __all__ = ["MaskModule"]
 
 
-def build_mask(s, margin=2, dtype=torch.float32):
-    mask = torch.zeros(1, 1, s, s, dtype=dtype)
+def build_mask(
+        s,
+        *,
+        dim: int = 2,
+        margin: float = 2.0,
+        sigma: float = 2.0,
+        dtype=torch.float32,
+):
+    mask = torch.zeros(1, 1, *repeat(s, dim), dtype=dtype)
     c = (s-1) / 2
     t = (c - margin/100.*c)**2
     sig = 2.
-    for x in range(s):
-        for y in range(s):
-            r = (x - c) ** 2 + (y - c) ** 2
-            if r > t:
-                mask[..., x, y] = math.exp((t - r)/sig**2)
-            else:
-                mask[..., x, y] = 1.
+    for k in product(range(s), repeat=dim):
+        r = sum((x - c)**2 for x in k)
+        if r > t:
+            mask[(..., *k)] = math.exp((t - r) / sigma**2)
+        else:
+            mask[(..., *k)] = 1.
     return mask
 
 
 class MaskModule(EquivariantModule):
 
-    def __init__(self, in_type: FieldType, S: int, margin: float = 0.):
+    def __init__(
+            self,
+            in_type: FieldType,
+            S: int,
+            margin: float = 0.,
+            sigma: float = 2.,
+    ):
         r"""
         
-        Performs an element-wise multiplication of the input with a *mask* of shape ``S x S``.
+        Performs an element-wise multiplication of the input with a *mask* of shape :math:`S^n`, where :math:`n` is the
+        dimensionality of the underlying space.
         
-        The mask has value :math:`1` in all pixels with distance smaller than ``(S-1)/2 * (1 - margin)/100`` from the
-        center of the mask and :math:`0` elsewhere. Values change smoothly between the two regions.
+        The mask has value :math:`1` in all pixels with distance smaller than
+        :math:`\frac{S - 1}{2} \times \frac{1 - \mathrm{margin}}{100}` from the center of the mask and :math:`0`
+        elsewhere. Values change smoothly between the two regions.
         
         This operation is useful to remove from an input image or feature map all the part of the signal defined on the
         pixels which lay outside the circle inscribed in the grid.
@@ -43,24 +58,35 @@ class MaskModule(EquivariantModule):
         discarded when rotating an image. However, allowing a model to use this information might break the guaranteed
         equivariance as rotated and non-rotated inputs have different information content.
         
-        
         .. note::
-        
-            In order to perform the masking, the module expects an input with the same spatial dimensions as the mask.
-            Then, input tensors must have shape ``B x C x S x S``.
-        
+            The input tensors provided to this module must have the following dimensions: :math:`B \times C \times S^n`,
+            where :math:`B` is the minibatch dimension, :math:`C` is the fiber group dimension, and :math:`S^n` are the
+            Euclidean dimensions associated with the given input field type.  Each Euclidean dimension must be of size
+            :math:`S`.
+
+            For example, if :math:`S=10` and the group is defined over :math:`\mathbb{R}^2`, then the input tensors
+            should be of size :math:`B \times C \times 10 \times 10`.  If the group were defined over
+            :math:`\mathbb{R}^3` instead, then the input tensors should be of size
+            :math:`B \times C \times 10 \times 10 \times 10`.
         
         Args:
             in_type (FieldType): input field type
             S (int): the shape of the mask and the expected inputs
             margin (float, optional): margin around the mask in percentage with respect to the radius of the mask
-                                      (default ``0.``)
+            sigma (float, optional): how quickly masked pixels should approach 0.  This can be thought of a standard
+                deviation in units of pixels/voxels.  For example, the default value of 2 means that only 5% of the
+                original signal will remain 4 px into the masked region.
         
         """
         super(MaskModule, self).__init__()
 
+        dim = in_type.gspace.dimensionality
+
         self.margin = margin
-        self.mask = torch.nn.Parameter(build_mask(S, margin=margin), requires_grad=False)
+        self.mask = torch.nn.Parameter(
+                build_mask(S, dim=dim, margin=margin, sigma=sigma),
+                requires_grad=False,
+        )
 
         self.in_type = self.out_type = in_type
 
