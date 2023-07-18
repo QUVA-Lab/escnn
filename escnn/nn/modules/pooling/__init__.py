388af7fa6898c8e7e82344cd98a16b11b3ebe1dd
chenhe <chenhe@megarobo.tech>
2023-07-18 17:40:12 +0800

    refactor, move pointwise_avg_3d into pointwise_avg
    Signed-off-by: chenhe <chenhe@megarobo.tech>

--- escnn/nn/modules/pooling/__init__.py
+++ escnn/nn/modules/pooling/__init__.py
@@ -4,15 +4,12 @@ from .pointwise_max import PointwiseMaxPool2D, PointwiseMaxPool
 from .pointwise_max import PointwiseMaxPoolAntialiased2D, PointwiseMaxPoolAntialiased
 from .pointwise_max import PointwiseMaxPool3D
 from .pointwise_max import PointwiseMaxPoolAntialiased3D
-from .pointwise_avg import PointwiseAvgPool, PointwiseAvgPool2D
-from .pointwise_avg import PointwiseAvgPoolAntialiased, PointwiseAvgPoolAntialiased2D
+from .pointwise_avg import PointwiseAvgPool, PointwiseAvgPool2D, PointwiseAvgPool3D
+from .pointwise_avg import PointwiseAvgPoolAntialiased, PointwiseAvgPoolAntialiased2D, PointwiseAvgPoolAntialiased3D
 from .pointwise_adaptive_avg import PointwiseAdaptiveAvgPool2D, PointwiseAdaptiveAvgPool
 from .pointwise_adaptive_avg import PointwiseAdaptiveAvgPool3D
 from .pointwise_adaptive_max import PointwiseAdaptiveMaxPool2D, PointwiseAdaptiveMaxPool
 from .pointwise_adaptive_max import PointwiseAdaptiveMaxPool3D
-
-from .pointwise_avg_3d import PointwiseAvgPool3D
-from .pointwise_avg_3d import PointwiseAvgPoolAntialiased3D
 
 
 __all__ = [
@@ -31,5 +28,3 @@ __all__ = [
     "PointwiseAvgPool3D",
     "PointwiseAvgPoolAntialiased3D",
 ]
-
-
