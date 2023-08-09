7f7b5485c8557eaca43666998aa127836ad672eb
Gabriele Cesa <gcesa@qti.qualcomm.com>
2023-08-09 13:26:20 +0200

    updated docs to v1.0.13

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.12',
+    VERSION: '1.0.13',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
